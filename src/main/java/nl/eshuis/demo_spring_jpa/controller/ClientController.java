976b9a05b7ffd96061f4b61c97014496321575ba
wilcolindeboom <wilco.lindeboom@gmail.com>
2021-01-13 00:49:19 +0100

    test

--- src/main/java/nl/eshuis/demo_spring_jpa/controller/ClientController.java
+++ src/main/java/nl/eshuis/demo_spring_jpa/controller/ClientController.java
@@ -11,11 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class ClientController {
